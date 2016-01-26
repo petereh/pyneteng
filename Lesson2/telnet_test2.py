d7c09ede5724a851d5320d72962bde9b333b8b2e
Peter <peterehiwe@gmail.com>
2016-01-26 08:57:22 -0800

    Ex 2

--- Lesson2/telnet_test2.py
+++ Lesson2/telnet_test2.py
@@ -2,6 +2,8 @@
 
 import telnetlib
 import time
+import socket
+import sys
 from getpass import getpass 
 
 
@@ -15,8 +17,11 @@ def telnet_cisco_device(username, password, remote_conn):
     
 
 def telnet_cursor_connect(ip, port=23, ttimeout=3):
-    remote_conn = telnetlib.Telnet(ip, port, ttimeout)
-    return remote_conn
+    try:
+        remote_conn = telnetlib.Telnet(ip, port, ttimeout)
+        return remote_conn
+    except (socket.error,AttributeError) :
+        sys.exit( "Error!!! Please Ensure you have entered the correct IP address , username and Password")
 
 def send_command(cmd, remote_conn) :
     remote_conn.write(cmd + '\n')
@@ -26,14 +31,16 @@ def send_command(cmd, remote_conn) :
 def main():
     USERN = raw_input("Enter Username:")
     PASSW = getpass("Enter Password:")
-    CONN = telnet_cursor_connect('50.76.53.27')
+    IP = raw_input("Enter IP address")
+    CONN = telnet_cursor_connect(IP)
     telnet_cisco_device(USERN, PASSW, CONN)
-    print CONN.read_very_eager()  
-    print send_command ("terminal length 0", CONN)
+    CONN.read_very_eager()  
+    send_command ("terminal length 0", CONN)
     print send_command ("show ip int br", CONN)
-    print CONN.read_very_eager()
-    print send_command ("terminal length 0", CONN)
+    CONN.read_very_eager()
+    send_command ("terminal length 0", CONN)
     print send_command ("show version", CONN)
+    CONN.close ()
 
 if __name__ == "__main__" :
     main()
